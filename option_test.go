c4a7693cc55d0e0ba11c66a893dbda0e1c79c447
goserg <gor-serg@bk.ru>
2023-05-28 20:58:57 +0300

    refactor tests; added tests for String() method

--- option_test.go
+++ option_test.go
@@ -1,8 +1,12 @@
 package optional
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestOptional(t *testing.T) {
+	t.Parallel()
 	type TestCase[T any] struct {
 		name       string
 		value      T
@@ -34,33 +38,62 @@ func TestOptional(t *testing.T) {
 		},
 	}
 	for _, tc := range tt {
-		var o Optional[int]
-		if tc.ok {
-			o = New(tc.value)
-		} else {
-			o = None[int]()
-		}
-		get, ok := o.Get()
-		Equal(t, tc.expectedOk, ok)
-		Equal(t, tc.expected, get)
-		if tc.expectedOk {
-			Equal(t, tc.expected, o.GetOrElse(777))
-		} else {
-			Equal(t, 777, o.GetOrElse(777))
-		}
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			var o Optional[int]
+			if tc.ok {
+				o = New(tc.value)
+			} else {
+				o = None[int]()
+			}
+			get, ok := o.Get()
+			Equal(t, tc.expectedOk, ok)
+			Equal(t, tc.expected, get)
+			if tc.expectedOk {
+				Equal(t, tc.expected, o.GetOrElse(777))
+			} else {
+				Equal(t, 777, o.GetOrElse(777))
+			}
 
-		if tc.expectedOk {
-			NotPanics(t, func() {
-				o.Must()
-			})
-		} else {
-			Panics(t, func() {
-				o.Must()
-			})
-			continue
-		}
-		Equal(t, tc.expected, o.Must())
+			if tc.expectedOk {
+				NotPanics(t, func() {
+					o.Must()
+				})
+			} else {
+				Panics(t, func() {
+					o.Must()
+				})
+				return
+			}
+			Equal(t, tc.expected, o.Must())
+		})
+
 	}
+}
+
+func TestOptional_String(t *testing.T) {
+	t.Parallel()
+	t.Run("string", func(t *testing.T) {
+		t.Parallel()
+		opStr := New("case1")
+		Equal(t, "Some(string)[case1]", opStr.String())
+	})
+	t.Run("string none", func(t *testing.T) {
+		t.Parallel()
+		noneSrt := None[string]()
+		Equal(t, "None(string)[]", noneSrt.String())
+	})
+	t.Run("Stringer", func(t *testing.T) {
+		t.Parallel()
+		opStringer := New(bytes.NewBufferString("case2"))
+		Equal(t, "Some(*bytes.Buffer)[case2]", opStringer.String())
+	})
+	t.Run("Stringer none", func(t *testing.T) {
+		t.Parallel()
+		noneStringer := None[*bytes.Buffer]()
+		Equal(t, "None(*bytes.Buffer)[]", noneStringer.String())
+	})
 }
 
 var x int
